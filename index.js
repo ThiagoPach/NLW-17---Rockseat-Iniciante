d6c00bff96b4a24228794c25da43b9293e127469
Thiago <Thiago.pacheco.c@gmail.com>
2024-09-14 16:31:40 -0300

    Menu da aplicação - Adicionar inquirer

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
 
     while(true){
-        let opcao = "sair"
+        
+        const opcao = await select ({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar metas",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar Metas",
+                    value: "listar metas"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+
+            ]
+        })
+
         switch(opcao) {
             case "cadastrar":
                 console.log("Vamos cadastrar")
