7961f534c4c6fbed683e22b7a10ea21c1252d487
Thiago <Thiago.pacheco.c@gmail.com>
2024-09-14 12:20:31 -0300

    Fundamentos 002

---
+++ index.js
@@ -0,0 +1,18 @@
+// Arrays, objetos
+let meta = {
+    value: 'ler um livro por mês',
+    address:2,
+    checked: true,
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.value = "Não é mais ler um livro"
+meta.log(meta.value)
+
+// function // arrow function
+//const criarMeta = () => {}
+
+//function criarMeta ()
