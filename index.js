6e4ca935615d7c9f6853c12541095ecd313a6174
Thiago <Thiago.pacheco.c@gmail.com>
2024-09-15 15:45:03 -0300

    Listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -16,15 +16,43 @@ const cadastrarMeta = async () => {
     }
 
     metas.push(
-        {value: meta, checked: false}
+        { value: meta, checked: false }
     )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa.",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if (respostas.length == 0) {
+        console.log("Nenhuma meta foi selecionada!")
+        return
+    }
+
+    metas.forEach((m) =>{
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+    })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como Concluída(s)')
+
 }
 
 const start = async () => {
 
-    while(true){
-        
-        const opcao = await select ({
+    while (true){
+
+        const opcao = await select({
             message: "Menu >",
             choices: [
                 {
@@ -33,7 +61,7 @@ const start = async () => {
                 },
                 {
                     name: "Listar Metas",
-                    value: "listar metas"
+                    value: "listar"
                 },
                 {
                     name: "Sair",
@@ -43,13 +71,13 @@ const start = async () => {
             ]
         })
 
-        switch(opcao) {
+        switch (opcao) {
             case "cadastrar":
                 await cadastrarMeta()
                 console.log(metas)
                 break
             case "listar":
-                console.log("Vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 return
